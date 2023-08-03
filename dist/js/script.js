ec90b283c77e4bbd32dfee5f22e8645e81b94464
Nyleco-J <anuran.nyleco24@gmail.com>
2023-08-03 12:36:25 +0800

    HeaderJavaUndone

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
-toggleBtn.addEventlistener('click',()=>{
-    drop.classlist.toggle('h-[20rem]');
+const header__actions = document.querySelector(".header__actions")
+const toggle__menu = document.querySelector(".toggle__menu")
+
+
+
+header__actions.addEventListener('click',()=>{
+    toggle__menu.classList.toggle('!h-[35vh]');
+    toggle__menu.classList.toggle('overflow-y-scroll');
 })
